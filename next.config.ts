3c160e5201556fea2c7a29b8e6501d824b7ed9f6
Xingdi Tan <steventanxd@gmail.com>
2025-06-18 16:17:42 -0700

    trivia

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "youtubeai.chat",
-          },
-        ],
-        destination: "https://www.youtubeai.chat/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
