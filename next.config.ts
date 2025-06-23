98020bb6b2134f584f65b254736af9c1cb791f71
Xingdi Tan <steventanxd@gmail.com>
2025-06-23 07:53:08 -0700

    fix build

--- next.config.ts
+++ next.config.ts
@@ -2,27 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      // Redirect from non-www to www
-      {
-        source: "http://youtubeai.chat/:path*",
-        destination: "https://www.youtubeai.chat/:path*",
-        permanent: true,
-      },
-      {
-        source: "https://youtubeai.chat/:path*",
-        destination: "https://www.youtubeai.chat/:path*",
-        permanent: true,
-      },
-      // Redirect from http www to https www
-      {
-        source: "http://www.youtubeai.chat/:path*",
-        destination: "https://www.youtubeai.chat/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
