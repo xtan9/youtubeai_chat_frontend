9c2dce17450813b0b6bab59196b2d553c321b6ab
Xingdi Tan <steventanxd@gmail.com>
2025-06-23 08:38:14 -0700

    seo: fix sitemap

--- middleware.ts
+++ middleware.ts
@@ -13,8 +13,10 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
+     * - sitemap.xml (for search engines)
+     * - robots.txt (for search engines)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
